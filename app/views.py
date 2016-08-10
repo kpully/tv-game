779e20128df55906e675f04bca0ac2aba3276d4f
Katherine Pully <kpully@gmail.com>
2016-08-10 00:34:34 -0400

    olympics

--- app/views.py
+++ app/views.py
@@ -10,14 +10,14 @@ import pandas as pd
 import numpy as np
 import random
 
-conditions = pd.read_csv('data/conditions.csv')
+
 alc_df = pd.read_csv('data/alc_general.csv')
 states_df = pd.read_csv('data/drunks.csv')
 
 alc_dict = alc_df.set_index('alc')['abv'].to_dict()
 states_dict = states_df.set_index('label')['bac'].to_dict()
 genders = {'male': 0.73, 'female': 0.66}
-conditions_dict = conditions.set_index('Condition')['times_per_show'].to_dict()
+
 
 
 #---------- URLS AND WEB PAGES -------------#
@@ -26,11 +26,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/_index', methods=["GET", "POST"])
+@app.route('/_bip', methods=["GET", "POST"])
 def get_form():
     form = MyForm()
-
-    alc = form.alc
+    show="Bachelor in Paradise"
     if request.method == "POST":
         name = request.form['firstname']
         name = name[0].upper()+name[1:]
@@ -50,15 +49,51 @@ def get_form():
             whiskey = True
         if request.form.get("beer"):
             beer = True
+        conditions = pd.read_csv('data/conditions.csv')
+        conditions_dict = conditions.set_index('Condition')['times_per_show'].to_dict()
         s = 0
         l = {}
         while s <= 15:
             c = random.choice(conditions_dict.items())
             l[c[0]]=c[1]
             s = s + c[1]
-        return render_template('results.html', name=name, conditions=json.dumps(l), weight=weight, gender=gender, drunk=drunk)
+        return render_template('results.html', name=name, conditions=json.dumps(l), weight=weight, gender=gender, drunk=drunk, show=show)
     elif request.method == "GET":
-        return render_template('index.html', form=form)
+        return render_template('bip.html', form=form)
 
+@app.route('/_olympics', methods=["GET", "POST"])
+def get_form_olympics():
+    form = MyForm()
+    show = "Olympics 2016"
+    if request.method == "POST":
+        name = request.form['firstname']
+        name = name[0].upper()+name[1:]
+        weight = request.form['weight']
+        gender = request.form['gender']
+        drunk = request.form['drunk']
+        redWine, whiteWine, vodka, champagne, whiskey, beer= False, False, False, False, False, False
+        if request.form.get("wine(red)"):
+            redWine = True
+        if request.form.get("wine(white)"):
+            whiteWine = True
+        if request.form.get("vodka"):
+            vodka = True
+        if request.form.get("champagne"):
+            champagne = True
+        if request.form.get("whiskey"):
+            whiskey = True
+        if request.form.get("beer"):
+            beer = True
+        conditions = pd.read_csv('data/olympics.csv')
+        conditions_dict = conditions.set_index('Condition')['times_per_show'].to_dict()
+        s = 0
+        l = {}
+        while s <= 25:
+            c = random.choice(conditions_dict.items())
+            l[c[0]]=c[1]
+            s = s + c[1]
+        return render_template('results.html', name=name, conditions=json.dumps(l), weight=weight, gender=gender, drunk=drunk, show=show)
+    elif request.method == "GET":
+        return render_template('olympics.html', form=form)
 
 
