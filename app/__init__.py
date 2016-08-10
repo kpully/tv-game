22afb7f7918bcbc43270478189732c983d07029f
Katherine Pully <kpully@gmail.com>
2016-08-09 22:13:21 -0400

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+ if __name__ == '__main__':
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
 app.config.from_object('config')
 
 from app import views
