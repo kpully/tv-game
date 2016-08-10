9c8f57ca47c648c0d03f8314d7b92ae39cbe673f
Katherine Pully <kpully@gmail.com>
2016-08-09 23:01:31 -0400

    run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 #!flask/bin/python
 from app import app
-app.run(debug=True)
+import os
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
