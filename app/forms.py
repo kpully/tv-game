ae70e3b94e9a04d72591c7b9773279934161f814
Katherine Pully <kpully@gmail.com>
2016-09-26 00:54:21 -0400

    debates

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, RadioField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, RadioField, BooleanField, IntegerField, SelectField, SelectMultipleField, SubmitField
+from wtforms.validators import DataRequired, Email, Regexp, EqualTo, Required
 
 class MyForm(Form):
     name = StringField('FirstName', validators=[DataRequired()])
-    weight = PasswordField('Weight', validators=[DataRequired()])
-    gender = RadioField('Gender', validators=[DataRequired()])
-    alc = BooleanField('Alcohol', validators=[DataRequired()])
+    weight = IntegerField('Weight', validators=[Required()])
+    time = SelectField('Time', choices=[('1', '1'), ('1.5', '1.5'), ('2', '2'), ('2.5', '2.5'), ('3', '3')])
+    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[DataRequired()])
+    alc = SelectField('Alcohol', choices=[('0.12', 'White Wine'), ('0.13', 'Red Wine'), ('0.4', 'Vodka'), ('0.12', 'Champagne'), ('0.4', 'Whiskey'), ('0.04', 'Beer')], validators=[DataRequired()])
+    drunk = SelectField('How drunk do you want to be?', choices=[('0.05', 'Buzzed'), ('0.1', 'Tipsy'), ('0.18', 'Hammered'), ('0.3', 'Blackout')], validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class OlympicForm(Form):
+    name = StringField('FirstName', validators=[DataRequired()])
+    weight = PasswordField('Weight', validators=[Required(), Regexp('^[0-9]$', 0, 'Weights must only have numbers')])
+    time = SelectField('Time', choices=[('1', '1'), ('1.5', '1.5'), ('2', '2'), ('2.5', '2.5'), ('3', '3')])
+    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[DataRequired()])
+    alc = SelectField('Alcohol', choices=[('0.12', 'White Wine'), ('0.13', 'Red Wine'), ('0.4', 'Vodka'), ('0.12', 'Champagne'), ('0.4', 'Whiskey'), ('0.04', 'Beer')], validators=[DataRequired()])
+    submit = SubmitField('Submit')
+	#sports = SelectMultipleField('Which events are you watching?', choices=[('general', 'Whatevers on'), ('swimming', 'Swimming'), ('gymnastics_women', 'Womens Gymnastics'), ('gymnastics_men', 'Mens Gymnastics'), ('track', 'Track'), ('basketball', 'Basketball'), ('field_hockey', 'Field Hockey'), ('fencing', 'Fencing')])
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request, redirect, flash, json
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-from .forms import MyForm
+from .forms import MyForm, OlympicForm
 from app import app
 
 import numpy as np
@@ -26,22 +26,21 @@ def index():
 
 
 @app.route('/_bip', methods=["GET", "POST"])
-def get_form():
+def get_form_bip():
     form = MyForm()
     show="Bachelor in Paradise"
-    if request.method == "POST":
-        name = request.form['firstname']
+    if form.validate_on_submit():
+        name = form.name.data
         name = name[0].upper()+name[1:]
-        weight = float(request.form['weight'])
-        hours = float(request.form['hours'])
+        weight = float(form.weight.data)
+        hours = float(form.time.data)
         gen_temp = request.form['gender']
         if gen_temp == 'male':
             gender = 0.73
         else:
             gender = 0.66
-        drunk = request.form['drunk']
-        alc = float(request.form['alc'])
-        state = float(request.form['drunk'])
+        state = float(form.drunk.data)
+        alc = float(form.alc.data)
         conditions = pd.read_csv('data/bip.csv')
         conditions_dict = conditions.set_index('Condition')['chance_of_condition'].to_dict()
         oz = math.floor((weight*gender*(state + 0.015*hours))/(5.14*alc))
@@ -61,12 +60,70 @@ def get_form():
                     imbibed = imbibed + 0.12*1.5
                     game[key] = "sip"
         return render_template('results.html', name=name, conditions=json.dumps(game), show=show, oz=oz)
-    elif request.method == "GET":
-        return render_template('bip.html', form=form)
+    return render_template('bip.html', form=form)
+
+@app.route('/_debate', methods=["GET", "POST"])
+def get_form_debate():
+    form = MyForm()
+    show="Presidential Debates"
+    if form.validate_on_submit():
+        name = form.name.data
+        name = name[0].upper()+name[1:]
+        weight = float(form.weight.data)
+        hours = float(form.time.data)
+        gen_temp = form.gender.data
+        if gen_temp == 'male':
+            gender = 0.73
+        else:
+            gender = 0.66
+        state = float(form.drunk.data)
+        alc = float(form.alc.data)
+        conditions = pd.read_csv('data/debates.csv')
+        conditions_dict = conditions.set_index('Condition')['chance_of_condition'].to_dict()
+        oz = math.floor((weight*gender*(state + 0.015*hours))/(5.14*alc))
+        keys = conditions_dict.keys()
+        random.shuffle(keys)
+        game = {}
+        imbibed = 0
+        for key in keys:
+            if int(imbibed) < oz:
+                if conditions_dict[key] == "low":
+                    imbibed = imbibed + 0.12*8
+                    game[key] = 'chug'
+                elif conditions_dict[key] == "normal":
+                    imbibed = imbibed + 0.12*3
+                    game[key] = "gulp"
+                elif conditions_dict[key] == "high":
+                    imbibed = imbibed + 0.12*1.5
+                    game[key] = "sip"
+        return render_template('results.html', name=name, conditions=json.dumps(game), show=show, oz=oz)
+    return render_template('debate.html', form=form)
+
+
+@app.route('/_create', methods=["GET", "POST"])
+def get_form_custom():
+    form = MyForm()
+    return render_template('custom.html', form=form)
+    if request.method == "POST":
+        show = request.form['show']
+        name = request.form['firstname']
+        name = name[0].upper()+name[1:]
+        weight = float(request.form['weight'])
+        hours = float(request.form['hours'])
+        gen_temp = request.form['gender']
+        if gen_temp == 'male':
+            gender = 0.73
+        else:
+            gender = 0.66
+        alc = float(request.form['alc'])
+        state = float(request.form['drunk'])
+        conditions=requests.form['custom']
+        oz = math.floor((weight*gender*(state + 0.015*hours))/(5.14*alc))
+        return render_template('results.html', name=name, conditions=json.dumps(game), show=show, oz=oz)
 
 @app.route('/_olympics', methods=["GET", "POST"])
 def get_form_olympics():
-    form = MyForm()
+    form = OlympicForm()
     show = "Olympics 2016"
     if request.method == "POST":
         name = request.form['firstname']
@@ -110,5 +167,6 @@ def get_form_olympics():
         return render_template('results.html', name=name, conditions=json.dumps(game), show=show, sports=sports, oz=oz)
     elif request.method == "GET":
         return render_template('olympics.html', form=form)
+
 
 
